fd6fee2af21db9c14fa0f6d7ebc87ede5b3bb944
Yury <123@mail.ru>
2017-09-08 22:26:06 +0400

    add song class

--- app/src/main/java/com/yurima/ksp/KSPSong.java
+++ app/src/main/java/com/yurima/ksp/KSPSong.java
@@ -1,8 +1,46 @@
 package com.yurima.ksp;
 
+import android.media.Image;
+
 /**
  * Created by Master on 9/8/2017.
  */
 
 public class KSPSong {
+
+    String mText;
+    String mTitle;
+    String mArtist;
+
+
+    public String getmText() {
+        return mText;
+    }
+
+    public void setmText(String mText) {
+        this.mText = mText;
+    }
+
+    public String getmTitle() {
+        return mTitle;
+    }
+
+    public void setmTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+
+    public String getmArtist() {
+        return mArtist;
+    }
+
+    public void setmArtist(String mArtist) {
+        this.mArtist = mArtist;
+    }
+
+
+    public KSPSong(){
+
+    }
+
+
 }
