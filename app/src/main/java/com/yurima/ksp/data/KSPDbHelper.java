931e741f44c655c9ad0b2386a57da6f40a06b655
Yury <31337@mail.ru>
2017-12-15 15:36:57 +0400

    fix DbHelper

--- app/src/main/java/com/yurima/ksp/data/KSPDbHelper.java
+++ app/src/main/java/com/yurima/ksp/data/KSPDbHelper.java
@@ -12,11 +12,12 @@ public class KSPDbHelper extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "KSPSongs.db";
     public static final int DATABASE_VERSION = 1;
 
-    private static final String SQL_CREATE_TABLE = "CREATE TABLE " + KSPSongEntry.TABLE_NAME + "(" +
-            KSPSongEntry._ID + "INTEGER PRIMARY KEY AUTOINCREMENT, " +
-            KSPSongEntry.COLUMN_TITLE + "TEXT NOT NULL, " +
-            KSPSongEntry.COLUMN_ATHOR + "TEXT NOT NULL" +
-            KSPSongEntry.COLUMN_TEXT + "TEXT";
+    private static final String SQL_CREATE_TABLE = "CREATE TABLE " + KSPSongEntry.TABLE_NAME + " (" +
+            KSPSongEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            KSPSongEntry.COLUMN_TITLE + " TEXT NOT NULL, " +
+            KSPSongEntry.COLUMN_ATHOR + " TEXT NOT NULL, " +
+            KSPSongEntry.COLUMN_TEXT + " TEXT" +
+            ");";
 
     private static final String SQL_DELETE_TABLE = "DROP TABLE " + KSPSongEntry.TABLE_NAME;
 
