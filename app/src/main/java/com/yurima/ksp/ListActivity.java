e8fba9c644d04330e67bbb929c1bad3d8f3b92cd
Yury <31337@mail.ru>
2017-12-15 18:03:54 +0400

    add Floating Button

--- app/src/main/java/com/yurima/ksp/ListActivity.java
+++ app/src/main/java/com/yurima/ksp/ListActivity.java
@@ -1,18 +1,13 @@
 package com.yurima.ksp;
 
-import android.content.Context;
-import android.database.Cursor;
-import android.support.v4.widget.CursorAdapter;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ListView;
-import android.widget.TextView;
-
 import com.yurima.ksp.adapters.KSPSongCursorAdapter;
-import com.yurima.ksp.data.KSPSongContract;
+
 
 public class ListActivity extends AppCompatActivity {
 
@@ -20,6 +15,16 @@ public class ListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        FloatingActionButton floatingActionButton = (FloatingActionButton)
+                findViewById(R.id.floating_action_button);
+        floatingActionButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ListActivity.this, DetailActivity.class);
+                startActivity(intent);
+            }
+        });
 
         ListView listView = (ListView) findViewById(R.id.song_list_view);
         KSPSongCursorAdapter adapter = new KSPSongCursorAdapter(this, null);
